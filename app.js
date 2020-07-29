ad74b355685784250c52ff7149c2be14f5563724
kxd <475922766@qq.com>
2020-07-29 15:55:16 +0800

    页面生命周期函数

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ App({
   //  1. 应用第一次启动的就会触发的事件
   onLaunch() {
     // 在应用第一次启动的时候，获取用户的个人信息
-    console.log("onLaunch");
+    // console.log("onLaunch");
     // aabbcc
 
     // js的方式来跳转，不能触发 onPageNotFound
@@ -15,20 +15,20 @@ App({
   // 2. 应用 被用户看到 时触发
   onShow() {
     // 对应用的数据 或者 页面效果 重置
-    console.log("onShow");
+    // console.log("onShow");
   },
 
   // 3. 应用 被隐藏 时触发
   onHide() {
     // 暂停 或者 清除 定时器
-    console.log("onHide");
+    // console.log("onHide");
   },
 
   // 4. 应用的代码发生了报错的时候，就会触发
   onError(err) {
     // 在应用发生代码报错的时候，收集用户的错误信息，通过异步请求，将错误的信息发送到后台去
-    console.log("onError");
-    console.log(err);
+    // console.log("onError");
+    // console.log(err);
   },
 
   // 5. 页面找不到就会触发
